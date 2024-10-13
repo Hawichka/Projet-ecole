46ef6b6bf997c1f9cdd91f26dcc849c0bf5ee265
Hawichka <hawac39@gmail.com>
2024-10-13 17:38:19 +0000

    j'ai apportée des modification au niveau du script pour la validation du formulaire et ajouter un fichier page2

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
 var formulaire = document.getElementById('envoyer');
 var password = document.getElementById('password');
 
+=======
+let formulaire = document.getElementById('formulaire');
+
+
+
+formulaire.addEventListener('submit', function(e){
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+    if (email.value ==""){
+        let erreur = document.getElementById('erreur');
+        erreur.innerHTML = "Email incorrect";
+        erreur.style.color = 'red';
+        e.preventDefault();
+    }
+    if(password.value==""){
+        let mdp = document.getElementById('mdp');
+        mdp.innerHTML = "mot de passe incorrect";
+        mdp.style.color = 'red'
+        e.preventDefault();
+    }
+})
+>>>>>>> c691b59 (j'ai apportée des modification au niveau du script pour la validation du formulaire et ajouter un fichier page2)
